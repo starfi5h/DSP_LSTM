e7f0991bcc8a8da701d95d83655703c86e217366
starfish <ac355deny@gmail.com>
2022-03-26 00:43:18 +0800

    Replace AccessTools

--- LSTM/UIStatisticsWindowAgent.cs
+++ LSTM/UIStatisticsWindowAgent.cs
@@ -130,10 +130,10 @@ namespace LSTMMod
             UIStatisticsWindow statisticsWindow = UIRoot.instance.uiGame.statWindow;
             UIStatisticsWindowAgent agent = statisticsWindow.gameObject.AddComponent<UIStatisticsWindowAgent>();
             agent.window = statisticsWindow;
-            agent.entriesLen = AccessTools.FieldRefAccess<UIStatisticsWindow, int>(statisticsWindow, "entriesLen");
+            agent.entriesLen = statisticsWindow.entriesLen;
             agent.entries = new UIProductEntryAgent[agent.entriesLen];
 
-            UIProductEntry[] ary = AccessTools.FieldRefAccess<UIStatisticsWindow, UIProductEntry[]>(statisticsWindow, "entries");
+            UIProductEntry[] ary = statisticsWindow.entries;
             for (int i = 0; i < agent.entriesLen; i++)
             {
                 agent.entries[i] = ary[i]?.gameObject.GetComponent<UIProductEntryAgent>();
